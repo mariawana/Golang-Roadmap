d80e7f47247e52b4b1a8488eb8751ccf651dc9f3
mariawana <araomariella08@gmail.com>
2026-03-24 08:00:33 +0800

    Update basic_data_types.go

--- Iteration 1 - Fundamentals/basic_data_types.go
+++ Iteration 1 - Fundamentals/basic_data_types.go
@@ -1,1 +1,26 @@
+package main
 
+import "fmt"
+
+func main () {
+
+  // integer
+  var score int = 67
+
+  // float
+  var grade float64 = 79.5
+
+  // string
+  var name string = "Maria Juana"
+
+  // boolean
+  var isPassed bool = true
+
+  fmt.Println("score:", score)
+  fmt.Println("grade:", grade)
+  fmt.Println("name:", name)
+  fmt.Println("is passed?", isPassed)
+
+}
+
+  
